a9a2891718e0073d6e7b7c2e89e9d5556ec11a23
lucas <lucas.scurvello@gmail.com>
2023-06-21 12:09:21 -0300

    finishing home interface

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,12 @@
+import { FiPlus } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
 import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Note } from '../../components/Note';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home() {
   return (
@@ -9,20 +15,36 @@ export function Home() {
         <h1>Rocketnotes</h1>
       </Brand>
 
+      <Header />
+
       <Menu>
+        <li><ButtonText title="Todos" /></li>
+        <li><ButtonText title="React" /></li>
+        <li><ButtonText title="Nodejs" /></li>
 
       </Menu>
 
       <Search>
-
+        <Input placeholder="Pesquisar pelo título"/>
       </Search>
 
       <Content>
+        <Section title="Minhas notas">
+          <Note data={{
+            title: 'React', 
+            tags: [
+              {id: '1', name: 'react' }, 
+              {id: '2', name: 'rocketseat' }
+            ]
+          }}
+          />
 
+        </Section>
       </Content>
 
       <NewNote>
-        
+        <FiPlus />
+        Criar Nota
       </NewNote>
 
     </Container>
