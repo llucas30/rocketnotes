2d69431f70d99b2be23860d7284881bbcf0b27d8
lucas <lucas.scurvello@gmail.com>
2023-06-23 14:19:51 -0300

    navigation

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
@@ -16,7 +17,7 @@ export function New() {
         <Form>
           <header>
             <h1>Criar nota</h1>
-            <a href="/">voltar</a>
+            <Link to="/">voltar</ Link>
           </header>
 
           <Input placeholder="Título" />
