64c30e7e46876dc6ecad7b321f3e8714f4d4457e
lucas <lucas.scurvello@gmail.com>
2023-06-23 14:08:07 -0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components';
+
+import { Routes } from './routes'
 
 import theme from './styles/theme'
-
-import { New } from './pages/New'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme = {theme}>
       <GlobalStyles />
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )
